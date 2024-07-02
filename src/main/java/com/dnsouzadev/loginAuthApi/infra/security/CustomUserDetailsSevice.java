bd3f46d3f9e99f47a8ca00ed5349de9683b136fc
Daniel Souza <workdndsza@gmail.com>
2024-07-02 14:09:01 -0300

    finalizando a auth da aplicacao

---
+++ src/main/java/com/dnsouzadev/loginAuthApi/infra/security/CustomUserDetailsSevice.java
@@ -0,0 +1,24 @@
+package com.dnsouzadev.loginAuthApi.infra.security;
+
+import com.dnsouzadev.loginAuthApi.domain.user.User;
+import com.dnsouzadev.loginAuthApi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class CustomUserDetailsSevice implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
