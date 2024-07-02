ee9d91147d84a7d6a690a56d864ab13fa8bfe7de
Daniel Souza <workdndsza@gmail.com>
2024-07-02 13:57:55 -0300

    security filter para filtrar a autenticacao

--- src/main/java/com/dnsouzadev/loginAuthApi/repositories/UserRepository.java
+++ src/main/java/com/dnsouzadev/loginAuthApi/repositories/UserRepository.java
@@ -3,5 +3,8 @@ package com.dnsouzadev.loginAuthApi.repositories;
 import com.dnsouzadev.loginAuthApi.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
